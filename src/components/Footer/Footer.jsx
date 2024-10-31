199c40e58a49c22d2954b977e7c61c7248742c7b
AJAY-CHAUHAN0001 <ajchauhan75365@gmail.com>
2024-10-31 19:29:19 +0530

    new one

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -4,7 +4,7 @@ import './Footer.css';
 export const Footer = () => {
   return (
     <footer className="footer">
-        Copyright &#169; 2024 Ajay Kumar. All rights reserved.
+        Copyright &#169; 2024 Ajay. All rights reserved.
     </footer>
   )
 }
